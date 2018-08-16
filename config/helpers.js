f84b9dad744c9477e0c5cb69aab7baf88519539f
wmlzjb <wmlzjb@aliyun.com>
2018-08-16 22:36:32 +0800

    修改webpack配置，使用 angular-starter 模板

--- config/helpers.js
+++ config/helpers.js
@@ -1,10 +1,31 @@
-var path = require('path');
+/**
+ * @author: tipe.io
+ */
+const path = require('path');
 
-var _root = path.resolve(__dirname, '..');
+const EVENT = process.env.npm_lifecycle_event || '';
 
-function root(args) {
-    args = Array.prototype.slice.call(arguments, 0);
-    return path.join.apply(path, [_root].concat(args));
+/**
+ * Helper functions.
+ */
+var ROOT = path.resolve(__dirname, '..');
+
+function hasProcessFlag(flag) {
+    return process.argv.join('').indexOf(flag) > -1;
 }
 
+function hasNpmFlag(flag) {
+    return EVENT.includes(flag);
+}
+
+function isWebpackDevServer() {
+    return process.argv[1] && !!(/webpack-dev-server/.exec(process.argv[1]));
+}
+
+
+var root = path.join.bind(path, ROOT);
+
+exports.hasProcessFlag = hasProcessFlag;
+exports.hasNpmFlag = hasNpmFlag;
+exports.isWebpackDevServer = isWebpackDevServer;
 exports.root = root;
